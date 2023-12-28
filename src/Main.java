a1157a1e9b22ac8d7df45c65fe07e9f200129f47
ArmandoBoaca <147373751+ArmandoBoaca@users.noreply.github.com>
2023-12-28 18:34:43 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+/*
+Scrivi un programma che sia in grado di leggere un file di testo nel
+quale siano contenute una serie di stringhe su più righe consecutive,
+ad es.
+alpha
+bravo
+charlie
+delta
+echo
+foxtrot
+lo stesso programma deve permettere di modificare una delle stringhe salvando successivamente sullo stesso file, il cambiamento.
+*/
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int nParam = 1;
+        if (args.length >= nParam){
+            File input  = new File
+        }
     }
 }
